8522e5b3c5795b5ec207ca45ef1b1fd27106b915
Zeth <theology@gmail.com>
2016-06-17 05:30:58 +0100

    Mac Keyboard support.

--- inputs.py
+++ inputs.py
@@ -1206,6 +1206,129 @@ MAC_EVENT_CODES = (
     (34, (None, 0, 0, 0)),   # NSEventTypePressure
 )
 
+MAC_KEYS = (
+    (0x00, 30),  # kVK_ANSI_A
+    (0x01, 31),  # kVK_ANSI_S
+    (0x02, 32),  # kVK_ANSI_D
+    (0x03, 33),  # kVK_ANSI_F
+    (0x04, 35),  # kVK_ANSI_H
+    (0x05, 34),  # kVK_ANSI_G
+    (0x06, 44),  # kVK_ANSI_Z
+    (0x07, 45),  # kVK_ANSI_X
+    (0x08, 46),  # kVK_ANSI_C
+    (0x09, 47),  # kVK_ANSI_V
+    (0x0B, 48),  # kVK_ANSI_B
+    (0x0C, 16),  # kVK_ANSI_Q
+    (0x0D, 17),  # kVK_ANSI_W
+    (0x0E, 18),  # kVK_ANSI_E
+    (0x0F, 33),  # kVK_ANSI_R
+    (0x10, 21),  # kVK_ANSI_Y
+    (0x11, 20),  # kVK_ANSI_T
+    (0x12, 2),  # kVK_ANSI_1
+    (0x13, 3),  # kVK_ANSI_2
+    (0x14, 4),  # kVK_ANSI_3
+    (0x15, 5),  # kVK_ANSI_4
+    (0x16, 7),  # kVK_ANSI_6
+    (0x17, 6),  # kVK_ANSI_5
+    (0x18, 13),  # kVK_ANSI_Equal
+    (0x19, 10),  # kVK_ANSI_9
+    (0x1A, 8),  # kVK_ANSI_7
+    (0x1B, 12),  # kVK_ANSI_Minus
+    (0x1C, 9),  # kVK_ANSI_8
+    (0x1D, 11),  # kVK_ANSI_0
+    (0x1E, 27),  # kVK_ANSI_RightBracket
+    (0x1F, 24),  # kVK_ANSI_O
+    (0x20, 22),  # kVK_ANSI_U
+    (0x21, 26),  # kVK_ANSI_LeftBracket
+    (0x22, 23),  # kVK_ANSI_I
+    (0x23, 25),  # kVK_ANSI_P
+    (0x25, 38),  # kVK_ANSI_L
+    (0x26, 36),  # kVK_ANSI_J
+    (0x27, 40),  # kVK_ANSI_Quote
+    (0x28, 37),  # kVK_ANSI_K
+    (0x29, 39),  # kVK_ANSI_Semicolon
+    (0x2A, 43),  # kVK_ANSI_Backslash
+    (0x2B, 51),  # kVK_ANSI_Comma
+    (0x2C, 53),  # kVK_ANSI_Slash
+    (0x2D, 49),  # kVK_ANSI_N
+    (0x2E, 50),  # kVK_ANSI_M
+    (0x2F, 52),  # kVK_ANSI_Period
+    (0x32, 41),  # kVK_ANSI_Grave
+    (0x41, 83),  # kVK_ANSI_KeypadDecimal
+    (0x43, 55),  # kVK_ANSI_KeypadMultiply
+    (0x45, 78),  # kVK_ANSI_KeypadPlus
+    (0x47, 69),  # kVK_ANSI_KeypadClear
+    (0x4B, 98),  # kVK_ANSI_KeypadDivide
+    (0x4C, 96),  # kVK_ANSI_KeypadEnter
+    (0x4E, 74),  # kVK_ANSI_KeypadMinus
+    (0x51, 117),  # kVK_ANSI_KeypadEquals
+    (0x52, 82),  # kVK_ANSI_Keypad0
+    (0x53, 79),  # kVK_ANSI_Keypad1
+    (0x54, 80),  # kVK_ANSI_Keypad2
+    (0x55, 81),  # kVK_ANSI_Keypad3
+    (0x56, 75),  # kVK_ANSI_Keypad4
+    (0x57, 76),  # kVK_ANSI_Keypad5
+    (0x58, 77),  # kVK_ANSI_Keypad6
+    (0x59, 71),  # kVK_ANSI_Keypad7
+    (0x5B, 72),  # kVK_ANSI_Keypad8
+    (0x5C, 73),  # kVK_ANSI_Keypad9
+    (0x24, 28),  # kVK_Return
+    (0x30, 15),  # kVK_Tab
+    (0x31, 57),  # kVK_Space
+    (0x33, 111),  # kVK_Delete
+    (0x35, 1),  # kVK_Escape
+    (0x37, 125),  # kVK_Command
+    (0x38, 42),  # kVK_Shift
+    (0x39, 58),  # kVK_CapsLock
+    (0x3A, 56),  # kVK_Option
+    (0x3B, 29),  # kVK_Control
+    (0x3C, 54),  # kVK_RightShift
+    (0x3D, 100),  # kVK_RightOption
+    (0x3E, 126),  # kVK_RightControl
+    (0x36, 126),  # Right Meta
+    (0x3F, 0x1d0),  # kVK_Function
+    (0x40, 187),  # kVK_F17
+    (0x48, 115),  # kVK_VolumeUp
+    (0x49, 114),  # kVK_VolumeDown
+    (0x4A, 113),  # kVK_Mute
+    (0x4F, 188),  # kVK_F18
+    (0x50, 189),  # kVK_F19
+    (0x5A, 190),  # kVK_F20
+    (0x60, 63),  # kVK_F5
+    (0x61, 64),  # kVK_F6
+    (0x62, 65),  # kVK_F7
+    (0x63, 61),  # kVK_F3
+    (0x64, 66),  # kVK_F8
+    (0x65, 67),  # kVK_F9
+    (0x67, 87),  # kVK_F11
+    (0x69, 183),  # kVK_F13
+    (0x6A, 186),  # kVK_F16
+    (0x6B, 184),  # kVK_F14
+    (0x6D, 68),  # kVK_F10
+    (0x6F, 88),  # kVK_F12
+    (0x71, 185),  # kVK_F15
+    (0x72, 138),  # kVK_Help
+    (0x73, 102),  # kVK_Home
+    (0x74, 104),  # kVK_PageUp
+    (0x75, 111),  # kVK_ForwardDelete
+    (0x76, 62),  # kVK_F4
+    (0x77, 107),  # kVK_End
+    (0x78, 60),  # kVK_F2
+    (0x79, 109),  # kVK_PageDown
+    (0x7A, 59),  # kVK_F1
+    (0x7B, 105),  # kVK_LeftArrow
+    (0x7C, 106),  # kVK_RightArrow
+    (0x7D, 108),  # kVK_DownArrow
+    (0x7E, 103),  # kVK_UpArrow
+    (0x0A, 170),  # kVK_ISO_Section
+    (0x5D, 124),  # kVK_JIS_Yen
+    (0x5E, 92),  # kVK_JIS_Underscore
+    (0x5F, 95),  # kVK_JIS_KeypadComma
+    (0x66, 94),  # kVK_JIS_Eisu
+    (0x68, 90)   # kVK_JIS_Kana
+)
+
+
 # We have yet to support force feedback but probably should
 # eventually:
 
@@ -1382,6 +1505,8 @@ class BaseListener(object):
             "Absolute": 0x03,
             "Misc": 0x04
         }
+        if MAC:
+            self.mac_codes = dict(MAC_KEYS)
         if not getattr(self, "codes", None):
             self.codes = None
         self.install_handle_input()
@@ -2037,6 +2162,101 @@ def mac_mouse_process(pipe):
     mouse = MacMouseEvdev(pipe)
 
 
+def mac_keyboard_process(pipe):
+    """Single subprocesses for reading keyboard on Mac."""
+    # pylint: disable=import-error,too-many-locals
+    # Note Objective C does not support a Unix style fork.
+    # So these imports have to be inside the child subprocess since
+    # otherwise the child process cannot use them.
+
+    from AppKit import NSApplication, NSApp
+    from Foundation import NSObject, NSLog
+    from Cocoa import (NSEvent, NSKeyDownMask, NSKeyUpMask,
+                       NSFlagsChangedMask)
+    from PyObjCTools import AppHelper
+
+    class MacKeyboardSetup(NSObject):
+        """Setup the handler."""
+        def init_with_handler(self, handler):
+            """
+            Init method that receives the write end of the pipe.
+            """
+            # ALWAYS call the super's designated initializer.
+            # Also, make sure to re-bind "self" just in case it
+            # returns something else!
+            self = super(MacKeyboardSetup, self).init()
+
+            self.handler = handler
+
+            # Unlike Python's __init__, initializers MUST return self,
+            # because they are allowed to return any object!
+            return self
+
+        # pylint: disable=invalid-name, unused-argument
+        def applicationDidFinishLaunching_(self, notification):
+            """Bind the handler to listen to keyboard events."""
+            mask = NSKeyDownMask | NSKeyUpMask | NSFlagsChangedMask
+            NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(
+                mask, self.handler)
+
+    class MacKeyboardEvdev(BaseListener):
+        """Loosely emulate Evdev mouse behaviour on the Mac Windows.
+        Listen for key events then buffer them in a pipe.
+        """
+        def __init__(self, pipe):
+            super(MacKeyboardEvdev, self).__init__(pipe)
+            self.codes = dict(MAC_EVENT_CODES)
+
+        def install_handle_input(self):
+            """Install the hook."""
+            self.app = NSApplication.sharedApplication()
+            # pylint: disable=no-member
+            delegate = MacKeyboardSetup.alloc().init_with_handler(
+                self.handle_input)
+            NSApp().setDelegate_(delegate)
+            AppHelper.runEventLoop()
+
+        def __del__(self):
+            """Stop the listener on deletion."""
+            AppHelper.stopEventLoop()
+
+        def handle_input(self, event):
+            """Process they keyboard input."""
+            timeval = self.get_timeval()
+            events = []
+            key_code = event.keyCode()
+            if key_code in self.mac_codes:
+                new_code = self.mac_codes[key_code]
+            else:
+                new_code = 0
+            event_type = event.type()
+            if event_type == 10:
+                value = 1
+            elif event_type == 11:
+                value = 0
+            elif event_type == 12:
+                flags = event.modifierFlags()
+                # Note, this may be able to be made more accurate,
+                # i.e. handle two modifier keys at once.
+                if flags == 0x100:
+                    value = 0
+                else:
+                    value = 1
+            else:
+                value = -1
+            scan_event, key_event = self.emulate_press(
+                new_code, key_code, value, timeval)
+
+            events.append(scan_event)
+            events.append(key_event)
+            # End with a sync marker
+            events.append(self.sync_marker(timeval))
+            # We are done
+            self.write_to_pipe(events)
+
+    keyboard = MacKeyboardEvdev(pipe)
+
+
 class InputDevice(object):
     """A user input device."""
     # pylint: disable=too-many-instance-attributes
@@ -2183,11 +2403,14 @@ class Keyboard(InputDevice):
     @staticmethod
     def _get_target_function():
         """Get the correct target function."""
-        return keyboard_process
+        if WIN:
+            return keyboard_process
+        elif MAC:
+            return mac_keyboard_process
 
     def _get_data(self, read_size):
         """Get data from the character device."""
-        if not WIN:
+        if NIX:
             return super(Keyboard, self)._get_data(read_size)
         return self._pipe.recv_bytes()
 
