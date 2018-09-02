b533e6c9ad5f77ad668e9b709a9648fb462f6b5d
Zeth <theology@gmail.com>
2018-09-02 01:39:00 +0100

    JStest style example.

---
+++ examples/jstest.py
@@ -0,0 +1,112 @@
+"""Simple gamepad/joystick test example."""
+
+from __future__ import print_function
+
+
+from inputs import get_gamepad
+
+
+EVENT_ABB = (
+    ('Absolute-ABS_X', 'LX'),
+    ('Absolute-ABS_Y', 'LY'),
+    ('Absolute-ABS_RX', 'RX'),
+    ('Absolute-ABS_RY', 'RY'),
+    ('Absolute-ABS_Z', 'LZ'),
+    ('Absolute-ABS_RZ', 'RZ'),
+    ('Absolute-ABS_HAT0X', 'HX'),
+    ('Absolute-ABS_HAT0Y', 'HY'),
+    ('Key-BTN_NORTH', 'N'),
+    ('Key-BTN_EAST', 'E'),
+    ('Key-BTN_SOUTH', 'S'),
+    ('Key-BTN_WEST', 'W'),
+    ('Key-BTN_THUMBL', 'THL'),
+    ('Key-BTN_THUMBR', 'THR'),
+    ('Key-BTN_TL', 'TL'),
+    ('Key-BTN_TR', 'TR'),
+    ('Key-BTN_TL2', 'TL2'),
+    ('Key-BTN_TR2', 'TR3'),
+    ('Key-BTN_MODE', 'M'),
+    ('Key-BTN_START', 'ST'),
+    ('Key-BTN_SELECT', 'SL'),
+)
+
+
+# This is to reduce noise from the PlayStation controllers
+# For the Xbox controller, you can set this to 0
+MIN_ABS_DIFFERENCE = 5
+
+
+class JSTest(object):
+    """Simple joystick test class."""
+    def __init__(self):
+        self.btn_state = {}
+        self.old_btn_state = {}
+        self.abs_state = {}
+        self.old_abs_state = {}
+        self.abbrevs = dict(EVENT_ABB)
+        for key, value in self.abbrevs.items():
+            if key.startswith('Absolute'):
+                self.abs_state[value] = 0
+                self.old_abs_state[value] = 0
+            if key.startswith('Key'):
+                self.btn_state[value] = 0
+                self.old_btn_state[value] = 0
+
+    def process_event(self, event):
+        """Process the event into a state."""
+        if event.ev_type == 'Sync':
+            return
+        if event.ev_type == 'Misc':
+            return
+        key = event.ev_type + '-' + event.code
+        abbv = self.abbrevs[key]
+        if event.ev_type == 'Key':
+            self.old_btn_state[abbv] = self.btn_state[abbv]
+            self.btn_state[abbv] = event.state
+        if event.ev_type == 'Absolute':
+            self.old_abs_state[abbv] = self.abs_state[abbv]
+            self.abs_state[abbv] = event.state
+        self.output_state(event.ev_type, abbv)
+
+    def format_state(self):
+        """Format the state."""
+        output_string = ""
+        for key, value in self.abs_state.items():
+            output_string += key + ':' + '{:>4}'.format(str(value) + ' ')
+
+        for key, value in self.btn_state.items():
+            output_string += key + ':' + str(value) + ' '
+
+        return output_string
+
+    def output_state(self, ev_type, abbv):
+        """Print out the output state."""
+        if ev_type == 'Key':
+            if self.btn_state[abbv] != self.old_btn_state[abbv]:
+                print(self.format_state())
+                return
+
+        if abbv[0] == 'H':
+            print(self.format_state())
+            return
+
+        difference = self.abs_state[abbv] - self.old_abs_state[abbv]
+        if (abs(difference)) > MIN_ABS_DIFFERENCE:
+            print(self.format_state())
+
+    def process_events(self):
+        """Process available events."""
+        events = get_gamepad()
+        for event in events:
+            self.process_event(event)
+
+
+def main():
+    """Process all events forever."""
+    jstest = JSTest()
+    while 1:
+        jstest.process_events()
+
+
+if __name__ == "__main__":
+    main()
